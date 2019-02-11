596bea29b966f5b10eb44708597c830ad8fe4dff
maciej.wolejszo <maciej.wolejszo@gmail.com>
2019-02-12 00:50:38 +0100

    - Mocking in .NET Core Unit Tests with Moq: Getting Started -
    -- Mocking Method Calls --
    ---  Mocking Methods with out Parameters ---

--- 01AddTestProject/CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ 01AddTestProject/CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -97,5 +97,28 @@ namespace CreditCardApplications.Tests
 
             Assert.Equal(CreditCardApplicationDecision.ReferredToHuman,decision);
         }
+
+        [Fact]
+        public void DeclineLowIncomeApplicationsOutDemo()
+        {
+            Mock<IFrequentFlyerNumberValidator> mockValidator =
+                new Mock<IFrequentFlyerNumberValidator>();
+
+            bool isValid = true;
+            mockValidator.Setup(x => x.IsValid(It.IsAny<string>(), out isValid));
+
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
+
+            var application = new CreditCardApplication
+            {
+                GrossAnnualIncome = 19_999,
+                Age = 42,
+                FrequentFlyerNumber = "a"
+            };
+
+            CreditCardApplicationDecision decision = sut.EvaluateUsingOut(application);
+
+            Assert.Equal(CreditCardApplicationDecision.AutoDeclined, decision);
+        }
     }
 }
