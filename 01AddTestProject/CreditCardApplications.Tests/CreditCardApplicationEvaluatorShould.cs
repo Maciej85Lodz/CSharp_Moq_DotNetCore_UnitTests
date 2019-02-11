7457ba69abb6a1ea9a0547f40af8e3b07596b890
maciej.wolejszo <maciej.wolejszo@gmail.com>
2019-02-12 00:03:11 +0100

    - Mocking in .NET Core Unit Tests with Moq: Getting Started -
    -- Mocking Method Calls --
    ---  Configuring Mock Object Method Return Values ---

--- 01AddTestProject/CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ 01AddTestProject/CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -36,6 +36,26 @@ namespace CreditCardApplications.Tests
             Assert.Equal(CreditCardApplicationDecision.ReferredToHuman, decision);
         }
 
-        // other evaluator test conditions
+        [Fact]
+        public void DeclineLowIncomeApplications()
+        {
+            Mock<IFrequentFlyerNumberValidator> mockValidator =
+                new Mock<IFrequentFlyerNumberValidator>();
+
+            mockValidator.Setup(x => x.IsValid("x")).Returns(true);
+
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
+
+            var application = new CreditCardApplication
+            {
+                GrossAnnualIncome = 19_999,
+                Age = 42,
+                FrequentFlyerNumber = "x"
+            };
+
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+
+            Assert.Equal(CreditCardApplicationDecision.AutoDeclined, decision);
+        }
     }
 }
